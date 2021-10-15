cebe36e3aa47315da5df9d8becba5b18ccfb8699
Moises N <mnunez0224@gmail.com>
2021-10-15 00:09:09 -0500

    Frontend Styling & Organization

--- front-end/src/dashboard/ReservationComponent.js
+++ front-end/src/dashboard/ReservationComponent.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { updateReservationStatus } from "../utils/api";
 
-export default function ReservationComponent ({ reservation }) {
+export default function ReservationComponent ({ loadDashboard, reservation }) {
 
     if (!reservation || reservation.status === "finished") return null;
 
     const cancelMessage = () => {
         if (window.confirm("Do you want to cancel this reservation? This cannot be undone.")) {
-            window.location.reload();
+            
+            const abortController = new AbortController();
+
+            updateReservationStatus(reservation.reservation_id, "cancelled", abortController.status)
+                .then(loadDashboard)
+
+        return () => abortController.abort();
         }
     }
 
@@ -17,30 +24,30 @@ export default function ReservationComponent ({ reservation }) {
             <td>{reservation.first_name}</td>
             <td>{reservation.last_name}</td>
             <td>{reservation.mobile_number}</td>
-            <td>{reservation.reservation_time}</td>
+            <td>{reservation.reservation_date.substr(0, 10)}</td>
+            <td>{reservation.reservation_time.substr(0, 5)}</td>
             <td>{reservation.people}</td>
             
             <td data-reservation-id-status={reservation.reservation_id}>{reservation.status}</td>
 
+            {reservation.status === "booked" &&
+            <>
             <td>
-                <a href={`/reservations/${reservation.reservation_id}/edit`}>
-                        <button type="button">Edit</button>
+                <a href={`/reservations/${reservation_id}/seat`}>
+                    <button className="btn btn-light" type="button">Seat</button>
                 </a>
             </td>
-
             <td>
-                    <button type="button" onClick={cancelMessage} data-reservation-id-cancel={reservation.reservation_id}>
-                                Cancel
-                    </button>
+                <a href={`/reservations/${reservation.reservation_id}/edit`}>
+                    <button className="btn btn-secondary" type="button">Edit</button>
+                </a>
             </td>
-
-
-            {reservation.status === "booked" &&
-                <td>
-                    <a href={`/reservations/${reservation.reservation_id}/seat`}>
-                            <button type="button">Seat</button>
-                    </a>
-                </td>
+            <td>
+                <button className="btn btn-danger" type="button" onClick={cancelMessage} data-reservation-id-cancel={reservation.reservation_id}>
+                   Cancel
+                </button>
+            </td>
+            </>
             }
 
         </tr>       
